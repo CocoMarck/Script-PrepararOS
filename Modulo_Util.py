fb83ec13b8a6ae312ccbbd308e577eea0f4b716d
CocoMarck <76503881+CocoMarck@users.noreply.github.com>
2022-11-11 21:39:58 -0600

    Add files via upload

--- Modulo_Util.py
+++ Modulo_Util.py
@@ -2,51 +2,80 @@
 
 import os, platform
 
-def System():
-    sys = platform.system()
-    if sys == 'Windows': sys = 'win'
-    elif sys == 'Linux': sys = 'linux'
-    else: sys = 'linux'
-    return sys
+def System(opc = 'System'):
+    '''Comandos de sistema utiles'''
+    cmd = ''
+    if opc == 'System':
+        '''Devuelve el sistema operativo'''
+        cmd = platform.system()
+        if cmd == 'Windows': cmd = 'win'
+        elif cmd == 'Linux': cmd = 'linux'
+        else: cmd = 'linux' #(Mac)
+
+    elif opc == 'CleanScreen':
+        '''Limpia el texto/comandos que se muestren en la terminal'''
+        sys = System('System')
+        if sys == 'linux':
+            os.system('clear')
+        elif sys == 'win':
+            os.system('cls')
+        else: pass
+
+    elif opc == 'ShowArchive':
+        '''Muestra los archivos existentes en una ruta'''
+        sys = System('System')
+        if sys == 'linux':
+            os.system('ls')
+        elif sys == 'win':
+            os.system('dir')
+        else: pass
+
+    else: pass
+
+
+    return cmd
+
+def CleanScreen():
+    System('CleanScreen')
+
 
 sys = System()
 
-def Title(txt = '', smb = '#', see = True, spc = 4):
-    '''Mostrar un titulo'''
-    spc = ' '*spc
-    txt = f'{smb}{spc}{txt}{spc}{smb}'
-    if see == True:
-        print(txt)
-    elif see == False:
-        txt += '\n'
-    else:
-        pass
+
+def Show(opc = 'Title', txt = '', smb = '#', see = True, spc = 4):
+    '''Mostrar un texto predefinido'''
+    if opc == 'Title':
+        '''Mostrar un titulo'''
+        spc = ' '*spc
+        txt = f'{smb}{spc}{txt}{spc}{smb}'
+        if see == True:
+            print(txt)
+        elif see == False:
+            txt += '\n'
+        else:
+            pass
+
+    elif opc == 'Separator':
+        '''Para separar texto'''
+        txt = smb*spc
+        if see == True:
+            print(txt)
+        elif see == False:
+            txt += '\n'
+        else:
+            pass
+
+    else: txt = ''
+
+
     return txt
 
-def CleanScreen(sys=sys):
-    '''Limpiar pantalla'''
-    if sys == 'linux':
-        os.system('clear')
-    elif sys == 'win':
-        os.system('cls')
-    else: pass
+def Title(txt='', smb = '#', see = True, spc = 4):
+    txt = Show(opc='Title', txt=txt, smb=smb, see=see, spc=spc)
+    return txt
 
-def ShowArchive(sys=sys):
-    if sys == 'linux':
-        os.system('ls')
-    elif sys == 'win':
-        os.system('dir')
-    else: pass
-
-def Separator(spc = 128, smb = '#', see = True, sys=sys):
-    '''Separar texto'''
-    txt = smb*spc
-    if see == True:
-        print(txt)
-    elif see == False:
-        txt += '\n'
-    else:
-        pass
+def Separator(smb = '#', see = True, spc = 128):
+    txt = Show(opc='Separator', smb=smb, see=see, spc=spc)
     return txt
 
 def Continue(txt='¿Continuar?', lang = 'español', msg = False, sys=sys):
@@ -57,8 +86,8 @@ def Continue(txt='¿Continuar?', lang = 'español', msg = False, sys=sys):
 
     if msg == False:
         opc = input(f'{txt} {idm[0]}/{idm[1]}: ')
-        if opc == 's': CleanScreen(sys)
-        elif opc == 'n': CleanScreen(sys)
+        if opc == 's': System('CleanScreen')
+        elif opc == 'n': System('CleanScreen')
         elif opc == '':
             print('No escribiste nada')
             opc = Continue(txt=txt, lang=lang)
@@ -75,12 +104,15 @@ def Continue(txt='¿Continuar?', lang = 'español', msg = False, sys=sys):
 def Name(txt = 'Archivo', sys=sys):
     nme = input(Title(txt=f'Nombre de {txt}', see=False) +
               'Nombre: ')
-    CleanScreen(sys)
+    if nme == '':
+        nme ='No_name'
+    else: pass
+    CleanScreen()
     return nme
 
 def Path(txt = 'Ruta', sys=sys):
 #    pth = ''
-    CleanScreen(sys)
+    CleanScreen()
     pth_fin = ''
     opc = input(Title(txt=txt, see=False) +
         "¿Elegir ruta? s/n: ")
@@ -89,16 +121,25 @@ def Path(txt = 'Ruta', sys=sys):
 
         if opc == "s":
             pth = input("Escribe la ruta: ")
-        else:
-            pth = "$HOME/"
+        else: pth = "$HOME/"
+
     elif sys == 'win':
         pth_fin = '\\'
 
         if opc == "s":
             pth = input("Escribe la ruta: ")
-        else: pth = ''
+        else:
+            pth = (os.path.join(os.path.join(os.environ['USERPROFILE']),
+                   'Desktop'))
 
     else: pth = ''
+
+    if pth == '':
+        if sys == 'win':
+            pth = (os.path.join(os.path.join(os.environ['USERPROFILE']),
+                   'Desktop'))
+        elif sys == 'linux': pth = '$HOME/'
+        else: pass
 
     try: pth_laststr = pth[-1]
     except: pth_laststr = pth_fin
@@ -106,5 +147,5 @@ def Path(txt = 'Ruta', sys=sys):
     else:        
         pth = pth + pth_fin
 
-    CleanScreen(sys)
+    CleanScreen()
     return pth
